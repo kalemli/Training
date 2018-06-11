b0b8216f7afd4f0f93ac9d3fc70d936de1dcaef2
Atif Galamov <atif.g@bestcomp.net>
2018-06-11 19:08:37 +0400

    3. C# Guide

---
+++ source/3. C# Guide/SampleTestApp/SampleTestApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SampleTestApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleTestApp")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9cd7599-ba23-4d05-986f-b91554cc626a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ source/3. C# Guide/SampleTestApp/SampleTestApp/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleTestApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestChecked()
+        {
+            int maxValue = int.MaxValue;
+
+            Assert.AreEqual(unchecked(maxValue + 1), int.MinValue);
+
+            Assert.ThrowsException<OverflowException>(() =>
+            {
+                int overValue = checked(maxValue + 1);
+            });
+        }
+    }
+}
