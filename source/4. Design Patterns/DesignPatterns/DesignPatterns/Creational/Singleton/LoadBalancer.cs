d5e5e690ae8257d46cb50fc52cd219e5d4dbb21e
Atif Galamov <atif.g@bestcomp.net>
2018-07-07 15:43:13 +0400

    4. Design Patterns: Singleton

---
+++ source/4. Design Patterns/DesignPatterns/DesignPatterns/Creational/Singleton/LoadBalancer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public class LoadBalancer
+    {
+        // 1.
+        private static LoadBalancer _instance;
+        // 2.
+        private static object syncLock = new object();
+
+        protected LoadBalancer()
+        {
+            // 6.
+        }
+
+        public static LoadBalancer Instance()
+        {
+            if (_instance == null) // 3.
+            {
+                lock (syncLock) // 4.
+                {
+                    if (_instance == null) // 5.
+                    {
+                        _instance = new LoadBalancer();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
